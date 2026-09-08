aec161c0f091fd72d2d40cb4f32b907d8c9813fb
charan <saicharanbhonagiri02@gmail.com>
2026-09-08 11:32:35 +0530

    style

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Home from './pages/Home.jsx'
 import Login from './pages/Login.jsx'
 import Signup from './pages/Signup.jsx'
 import './App.css'
+import Navbar from './components/Navbar.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Login" element={<Login />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router'
+import './Navbar.css'
 
 function Navbar() {
   return (
-    <div>
-      
+    <div className='navbar'>
+     <Link to="/"> <h1>Logo</h1> </Link>
+      <ul>
+        <li>
+
+          <Link to="/Login">Login</Link>
+        </li>
+        <li>
+          <Link to="/Signup">Signup</Link>
+        </li>
+
+
+
+      </ul>
     </div>
   )
 }
